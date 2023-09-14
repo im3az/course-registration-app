3a072407caf8132a6815a4d8241c639620a816e5
Imtiaz <im3azchowdhury@gmail.com>
2023-09-15 01:41:53 +0600

    card section completed

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -4,16 +4,16 @@ const Card = ({ course }) => {
   const { title, image, details } = course;
 
   return (
-    <div >
-      <div className="card w-80 bg-base-100 shadow-xl">
-        <figure className="px-10 pt-10">
+    <div className="mb-3">
+      <div className="card w-[370px] h bg-base-100 shadow-xl">
+        <figure className="pt-5">
           <img src={image} alt="Course" className="w-72 h-36 rounded-xl" />
         </figure>
         <div className="card-body items-center text-center">
-          <h2 className="card-title">{title}</h2>
-          <p>{details}</p>
+          <h2 className="text-lg font-semibold">{title}</h2>
+          <h1 className="text-sm font-normal text-justify text-[#777676]">{details}</h1>
           <div className="card-actions">
-            <button className="btn btn-primary">Buy Now</button>
+            <button className="btn btn-primary mt-3 w-72">Select</button>
           </div>
         </div>
       </div>
