76b1bf6ce06c1aae7c98739dcac61875a7733227
Imtiaz <im3azchowdhury@gmail.com>
2023-09-15 10:08:33 +0600

    added price, credit and react icons

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,7 +1,8 @@
 import PropTypes from "prop-types";
+import { BsCurrencyDollar, BsBook } from "react-icons/bs";
 
 const Card = ({ course }) => {
-  const { title, image, details } = course;
+  const { title, image, details, price, credit_hour } = course;
 
   return (
     <div className="mb-3">
@@ -11,10 +12,22 @@ const Card = ({ course }) => {
         </figure>
         <div className="card-body items-center text-center">
           <h2 className="text-lg font-semibold">{title}</h2>
-          <h1 className="text-sm font-normal text-justify text-[#777676]">{details}</h1>
-          <div className="card-actions">
-            <button className="btn btn-primary mt-3 w-72">Select</button>
+          <h1 className="text-sm font-normal text-justify text-[#777676]">
+            {details}
+          </h1>
+        </div>
+        <div className="flex items-center justify-between w-4/5 mx-auto">
+          <div className="flex items-center gap-2">
+            <BsCurrencyDollar />
+            <h1 className="font-medium text-[#777676]">Price: {price}</h1>
           </div>
+          <div className="flex items-center gap-2">
+            <BsBook />
+            <h1 className="font-medium text-[#777676]">Credit: {credit_hour}hr</h1>
+          </div>
+        </div>
+        <div className="mb-5 items-center text-center">
+          <button className="btn btn-primary mt-3 w-72">Select</button>
         </div>
       </div>
     </div>
